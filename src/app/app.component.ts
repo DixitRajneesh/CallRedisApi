18458d38c5300a14f720aae8b174b29b7e28d778
Gitea <gitea@fake.local>
2021-12-07 18:58:12 +0530

    rajneesh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  apidata: any;
+  constructor(private http:HttpClient)
+  {
+
+  }
+  //this is my testing
   title = 'CallRedisApi';
+
+ngOnInit()
+{
+  debugger;
+this.BinddatafromRedisAPI();
 }
+
+
+BinddatafromRedisAPI()
+{
+  debugger;
+    return this.http.get("https://localhost:44300/api/RedisApi/getitem").subscribe
+    
+    ((resposne)=>
+    {this.apidata = resposne;
+      debugger;
+     console.log(resposne);
+    });
+}
+}
+
